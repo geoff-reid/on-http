aa13ea90f656208aa35a37729fcd66866cc4dd2c
Geoff Reid <geoffrey.reid@dell.com>
2017-01-09 11:56:42 -0500

    more debug

--- extra/upload-client.sh
+++ extra/upload-client.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 set -ex
 
+#index-servers =
+#  pypi
+#  pypitest
+
+#[pypi]
+#repository=https://pypi.python.org/pypi
+#username=
+#password=
+
 pwd ~
+
+if [ -f ~/.testfile ]
+then
+  cat ~/.testfile
+else
+  touch ~/.testfile
+fi
+
+echo "hi" >> ~/.testfile
+
+cat ~/.testfile
